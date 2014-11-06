6a18240f7b4cdb4d767314c6d8f38182d692ec76
Hugo Marinho <hm@12auto.de>
2014-11-06 17:32:48 +0100

    added testing function

--- pronfig.rb
+++ pronfig.rb
@@ -12,27 +12,40 @@ date_aliases = File.exist?(aliases_path) && File.mtime(aliases_path)
 exit 1 if date_aliases && (date_config < date_aliases)
 
 projects = YAML.load(File.read("#{pronfig_path}/config/config.yml"))
-aliases = "#!/bin/bash\n"
+aliases =  "#!/bin/bash\n"
 servers =  "ss() {\n"
 servers << "  case `basename $PWD` in\n"
+
+tests =  "t() {\n"
+tests << "  case `basename $PWD` in\n"
 
 projects.each do |p|
   aliases << "alias #{p["name"]}-folder=\"cd #{p["path"]}\"\n"
 
-  puts File.basename(p["path"])
-
-  servers << "  \"#{File.basename(p["path"])}\")                      echo_h1 \"Running #{p["name"]} port #{p["port"]}\n"
-  servers << "                                                        echo_h2 \"#{p["domain"]}\n"
+  servers << "  \"#{File.basename(p["path"])}\")                      echo_h1 \"Running #{p["name"]} port #{p["port"]}\"\n"
+  servers << "                                                        echo_h2 \"#{p["domain"]}\"\n"
   servers << "                                                        echo_separator\n"
   servers << "                                                        #{p["s"]}\n"
   servers << "                                                        ;;\n"
+
+  tests << "  \"#{File.basename(p["path"])}\")                      case $# in\n"
+  tests << "                                                          0) #{p["t"]};;\n"
+  tests << "                                                          1) #{p["t#1"]};;\n"
+  tests << "                                                          2) #{p["t#2"]};;\n"
+  tests << "                                                        esac\n"
+  tests << "                                                        ;;\n"
+
 end
   servers << "  *)                                                    echo_warning \"Not in a valid project folder.\""
-  servers << "                                                        ;;"
+  servers << "                                                        ;;\n"
   servers << "  esac\n"
-  servers << "}"
+  servers << "}\n"
 
+  tests << "  *)                                                    echo_warning \"Not in a valid project folder.\""
+  tests << "                                                        ;;\n"
+  tests << "  esac\n"
+  tests << "}\n"
 
-cmds = aliases + servers
+cmds = aliases + servers + tests
 
 File.open( pronfig_path + "/tmp/pronfig_aliases", 'w+') {|f| f.write(cmds) }
